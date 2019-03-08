03c60726252405e4ee9d2ba109438e00753334be
mattorus <mattorus@gmail.com>
2019-03-07 20:28:12 -0800

    Add student show page; Clean up code using partials.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,11 @@
 class StudentsController < ApplicationController
-
+  before_action :set_student, only: [:show, :edit, :update]
+  
   def index
     @students = Student.all
+  end
+
+  def show
   end
 
   def new
@@ -12,14 +16,31 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:notice] = "Sign up successful."
-      redirect_to root_path
+      redirect_to @student
     else
 
       render 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @student.update(student_params)
+      flash[:notice] = "Update successful"
+      # redirect_to student_path(@student)
+      redirect_to @student # Same functionality as line above
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
   def student_params
     params.require(:student).permit(:name, :email)
