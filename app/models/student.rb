f7eebcd3bd4b3f07a269a3df4eb7823d346d5c13
mattorus <mattorus@gmail.com>
2019-03-07 21:00:26 -0800

    Add student password functionality.

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,5 @@ class Student < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
