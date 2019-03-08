543c99baad701ce959769a53956a176df74fc574
mattorus <mattorus@gmail.com>
2019-03-07 19:38:06 -0800

    Add student sign up page & hook up functionality; Enable flash messaging.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,24 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
+  end
 
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "Sign up successful."
+      redirect_to root_path
+    else
+
+      render 'new'
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email)
   end
 
 end
